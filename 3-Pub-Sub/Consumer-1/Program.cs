7b4bd2bbb5fd76840c0ec2efedb2c4e03b004613
Ramesh Choudhary <rchoudhary@accoonaglobal.com>
2023-07-31 15:49:46 +0530

    Pub-Sub

---
+++ 3-Pub-Sub/Consumer-1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Uri uri = new Uri("broker-address:port");
+
+var factory = new ConnectionFactory
+{
+    HostName = "broker-address:port",
+    // UserName = "root",
+    // Password = "root"
+
+    UserName = "root",
+    Password = "root"
+};
+
+factory.Ssl.Enabled = true;
+factory.Uri = uri;
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "testPubSub", type: ExchangeType.Fanout);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queueName, exchange: "testPubSub", routingKey: "");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+    Console.WriteLine($"Consumer-1 => Message Recived => {message}");
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.ReadKey();
---
+++ 3-Pub-Sub/Producer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+Uri uri = new Uri("broker-address:port");
+
+var factory = new ConnectionFactory
+{
+    HostName = "broker-address:port",
+    UserName = "root",
+    Password = "root"
+};
+
+factory.Ssl.Enabled = true;
+factory.Uri = uri;
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "testPubSub", type: ExchangeType.Fanout);
+
+var message = "Broadcasting this message!!";
+
+var encodedMsg = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange:"testPubSub", "", null, encodedMsg);
+
+Console.WriteLine($"Published Message: {message}");
