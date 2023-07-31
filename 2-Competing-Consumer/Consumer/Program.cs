a446b59de4961495441e3f4e16cb99098108a9fe
Ramesh Choudhary <rchoudhary@accoonaglobal.com>
2023-07-31 15:49:29 +0530

    Competing Consumer Pattern

---
+++ 2-Competing-Consumer/Consumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Uri uri = new Uri("broker-address:port");
+
+var factory = new ConnectionFactory
+{
+    HostName = "broker-address:port",
+
+    UserName = "root",
+    Password = "root"
+};
+
+factory.Ssl.Enabled = true;
+factory.Uri = uri;
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "basicTest",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+var random = new Random();
+
+consumer.Received += (model, ea) =>
+{
+    var processingTime = random.Next(1, 6);
+
+    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+    Console.WriteLine($"Processed - {message} - Taking {processingTime} time to process.");
+
+    Task.Delay(TimeSpan.FromSeconds(processingTime)).Wait();
+
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+channel.BasicConsume(queue: "basicTest", autoAck: false, consumer: consumer);
+
+Console.ReadKey();
---
+++ 2-Competing-Consumer/Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+Uri uri = new Uri("broker-address:port");
+
+var factory = new ConnectionFactory
+{
+    HostName = "broker-address:port",
+    UserName = "root",
+    Password = "root"
+};
+
+factory.Ssl.Enabled = true;
+factory.Uri = uri;
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "basicTest", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var random = new Random();
+int msgId = 1;
+while (true)
+{
+    var pubTime = random.Next(1, 3);
+
+    var message = $"This is message {msgId}";
+
+    var encodedMsg = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "basicTest", null, encodedMsg);
+
+    Console.WriteLine($"Published Message: {message}");
+
+    await Task.Delay(TimeSpan.FromSeconds(pubTime));
+    
+    msgId++;
+}
